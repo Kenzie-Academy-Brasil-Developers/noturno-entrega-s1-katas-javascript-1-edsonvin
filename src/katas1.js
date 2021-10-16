5f8c83657c6f9fb7da38a26b30c0086fedb076e1
edson vinicius <edsonvin1801@gmail.com>
2021-10-15 21:13:53 -0300

    Entrega Edson

--- src/katas1.js
+++ src/katas1.js
@@ -1,94 +1,119 @@
+
+//function oneThroughTwenty
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
-}
-
-//call function oneThroughTwenty
-
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
   
+  for(let contador=1;contador<=20;contador++){
+    console.log(contador)
+  }
 }
+ oneThroughTwenty()
+  
+  
 
-//call function evensToTwenty
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+
+// function evensToTwenty() 
+function evensToTwenty() {
+  let counter = 1
+  while (counter<=20){
+         if(counter % 2 === 0){
+      console.log(counter)   
+      }
+      counter++}
+}
+ evensToTwenty()
+
+// function oddsToTwenty() 
    
-}
+function oddsToTwenty() {
+  let cont = 1
+  while(cont<=20){
+    if(cont % 2 != 0){
+      console.log(cont)
+    }
+    cont++
+  }
 
-//call function oddsToTwenty
+}
+oddsToTwenty()
+
+
+// function multiplesOfFive()
 
 function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contM5 = 1
+  while(contM5<=100){
+    if(contM5 % 5 === 0){
+      console.log(contM5)
+    }
+    contM5++
+  }
 
 }
+multiplesOfFive()
 
-//call function multiplesOfFive
 
-function squareNumbers() {
+// function squareNumbers() 
+  function squareNumbers(){
+  for(let contador = 1; contador <=10; contador++)
+  console.log((contador)**2)}
+  squareNumbers()
+
+// //call function squareNumbers
+
+// function countingBackwards() 
     
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+function countingBackwards(){
+  for(count = 20; count>=1; count--){
+    console.log(count)
+  }
 }
+countingBackwards()
 
-//call function squareNumbers
 
-function countingBackwards() {
+
+// function evenNumbersBackwards() {
+  function evenNumbersBackwards() {
+      let counterEB = 20
+      while (counterEB>=1){
+             if(counterEB % 2 === 0){
+          console.log(counterEB)   
+          }
+          counterEB--}
+    }
+    evenNumbersBackwards()
     
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
 
-//call function countingBackwards
+// oddNumbersBackwards()
+    function oddNumbersBackwards(){
+      contOB = 20
+      while(contOB>0){
+        if(contOB % 2 != 0){
+          console.log(contOB)
+        }
+        contOB--
+      }
+    }
+    oddNumbersBackwards()
 
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
 
-//call function evenNumbersBackwards
+// function multiplesOfFiveBackwards() {
+    function multiplesOfFiveBackwards(){
+      let contM5B = 100
+      while(contM5B>0){
+        if(contM5B % 5 === 0){
+          console.log(contM5B)
+        }
+        contM5B--
+      }
+    }
+    multiplesOfFiveBackwards()
 
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
+// function squareNumbersBackwards() {
+    function squareNumbersBackwards(){
+      for(let contSB = 10; contSB>0; contSB--){
+        console.log(contSB**2)
+      }
+    }
+    squareNumbersBackwards()
 
-//call function oddNumbersBackwards
-
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function squareNumbersBackwards
